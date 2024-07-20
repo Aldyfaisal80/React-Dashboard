de0abff48a5d50e4ec9a07c0442e16d7c3458b02
Aldyfaisal80 <98567736+Aldyfaisal80@users.noreply.github.com>
2024-07-20 20:15:56 +0700

    active aside link

--- src/elements/AsideLink.jsx
+++ src/elements/AsideLink.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { Link } from "react-router-dom";
+import { Link} from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
 export default function AsideLink(props) {
@@ -9,11 +9,11 @@ export default function AsideLink(props) {
     return (
         <Link
             aria-current="page"
-            className={`active ${isActive ? 'bg-blue-700' : ''}`}
+            className={`${isActive ? 'active bg-blue-700' : 'active'}`}
             to={props.href}
         >
             <button
-                className={`middle none font-sans font-bold center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 rounded-lg text-white   hover:bg-white/10 w-full flex items-center gap-4 px-4 capitalize ${isActive ? 'bg-blue-700' : ''}`}
+                className={`middle none font-sans font-bold center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 rounded-lg text-white hover:bg-white/10 w-full flex items-center gap-4 px-4 capitalize ${isActive ? 'bg-blue-700' : ''}`}
                 type="button"
             >
                 {props.children}
